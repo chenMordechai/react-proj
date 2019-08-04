801b9ada4d330d6ea29df35f662cb203211fd1a1
chen mordechai <chen100030@gmail.com>
2019-08-04 16:38:42 +0300

    Updates

---
+++ precache-manifest.fd0d967ca9de11fe1d4a43ea3ca55c1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "455f94ad00b349e9ca3c80d5074c4985",
+    "url": "​https://chenMordechai.github.io/react-proj/index.html"
+  },
+  {
+    "revision": "48455d15cd4af66641a5",
+    "url": "​https://chenMordechai.github.io/react-proj/static/css/main.88c35fa2.chunk.css"
+  },
+  {
+    "revision": "0795c4c75763c0736302",
+    "url": "​https://chenMordechai.github.io/react-proj/static/js/2.aea32ea8.chunk.js"
+  },
+  {
+    "revision": "48455d15cd4af66641a5",
+    "url": "​https://chenMordechai.github.io/react-proj/static/js/main.66e9125b.chunk.js"
+  },
+  {
+    "revision": "f28e6691e3ea309a68bd",
+    "url": "​https://chenMordechai.github.io/react-proj/static/js/runtime~main.a7040b77.js"
+  }
+]);
